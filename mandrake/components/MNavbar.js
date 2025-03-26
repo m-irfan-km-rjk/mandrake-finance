0973baec8c409c54fb08d9f432e9dd9bc4b1e628
Rjk 915 <mirfankm915@gmail.com>
2025-03-26 22:31:48 +0530

    Commit 2

--- mandrake/components/MNavbar.js
+++ mandrake/components/MNavbar.js
@@ -1,7 +1,15 @@
 function MNavbar() {
     return(
-        <div>
-            
+        <div className="h-[50px] bg-black w-full flex px-2 justify-between items-center">
+            <div className="flex gap-2">
+                <img src="/Mandrake.png" width={160} height={50} />
+            </div>
+            <ul className="flex font-bold justify-between w-[25%] text-lg text-white">
+                <li className="border-white border-l p-2">Home</li>
+                <li className="border-white border-l p-2">About Us</li>
+                <li className="border-white border-l p-2">Services</li>
+                <li className="border-white border-l p-2">Contact Us</li>
+            </ul>
         </div>
     );
 }
